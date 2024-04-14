81001351845ce3fe2842f6128d02c818914216ff
jmcada2503 <juanmiscj25@gmail.com>
2024-04-14 04:13:33 -0500

    plus: upload csv file

--- backend/main/api/urls.py
+++ backend/main/api/urls.py
@@ -3,4 +3,5 @@ from main.api.views import *
 
 urlpatterns = [
     path('user/', UserApiView.as_view(), name='user_api'),
+    path('upload_csv/', CsvFileApiView.as_view(), name='upload_csv_api'),
 ]
--- backend/main/api/views.py
+++ backend/main/api/views.py
@@ -6,6 +6,8 @@ from rest_framework.permissions import IsAuthenticated
 
 from main.api.serializers import *
 
+import csv
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -13,6 +15,19 @@ class UserApiView(APIView):
 
     @permission_classes([IsAuthenticated])
     def get(self, request):
-        logger.critical(f"request: {request.user}")
         user_serializer = UserSerializer(instance=request.user)
         return Response(user_serializer.data)
+
+class CsvFileApiView(APIView):
+
+  @permission_classes([IsAuthenticated])
+  def post(self, request):
+      if request.user.groups.filter(name__in=['administrator']).exists():
+        upload_serializer = UploadSerializer(data=request.data)
+        if (upload_serializer.is_valid()):
+          csv_file = upload_serializer.validated_data['csv_file']
+          lines = csv_file.readlines()
+          return Response({'msg': 'ok'})
+      else:
+        return Response(status=status.HTTP_403_FORBIDDEN)
+

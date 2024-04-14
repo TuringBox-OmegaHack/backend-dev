c7293fd32e5839b6da1b8d00939d51e1a32b93f1
jmcada2503 <juanmiscj25@gmail.com>
2024-04-14 05:58:03 -0500

    updated upload csv api

--- backend/main/api/views.py
+++ backend/main/api/views.py
@@ -27,7 +27,23 @@ class CsvFileApiView(APIView):
         if (upload_serializer.is_valid()):
           csv_file = upload_serializer.validated_data['csv_file']
           lines = csv_file.readlines()
-          return Response({'msg': 'ok'})
+          response = {}
+          devices = [i.decode('utf-8') for i in lines[0].split(b',')[3:]]
+          for device in devices:
+            response[device] = {
+              'points': [],
+              'avg': 0
+            }
+          for line in lines[1:]:
+            cols = line.split(b',')[3:]
+            for index, col in enumerate(cols):
+              response[devices[index]]['points'].append(float(col))
+              response[devices[index]]['avg'] += float(col)
+
+          for device in devices:
+            response[str(device)]['avg'] = response[device]['avg']/60
+
+          return Response(response)
       else:
         return Response(status=status.HTTP_403_FORBIDDEN)
 
